5be19d32c279f4e0a3c83ce61fb9a29cdc656ecd
Xikin <matheusfukinha64@hotmail.com>
2025-11-23 11:16:01 -0300

    fix cors

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import placeRouter from './routes/placeRouter.js';
 import mongoose from 'mongoose';
 import userRouter from './routes/userRouter.js';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -22,7 +23,7 @@ app.use((req, res, next) => {
   );
   next();
 });
-
+app.use(cors())
 app.use(express.json({ limit: '10mb' }));
 app.use('/user', userRouter);
 app.use('/place', placeRouter);
